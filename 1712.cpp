cf5d0ef8063b0ac0c847a4c2f92c580d7106697b
g_4ppLe <82714924+IcanBe4nyTh@users.noreply.github.com>
2021-12-28 08:45:44 +0900

    second_failed

--- 1712.cpp
+++ 1712.cpp
@@ -1,23 +1,23 @@
 // 백준 1712 손익분기점
-
 #include <iostream>
 using namespace std;
 
-unsigned long Break_Even_Point(unsigned long a,unsigned long b,unsigned long c){
-	unsigned long total_income=0;
-	unsigned long total_cost=0;
+long Break_Even_Point(long a,long b,long c){
+	long result=0;
 	if (b>=c) return -1;
-	for(unsigned long i=0;;i++){
-		total_income=c*i;
-		total_cost=a+b*i;
-		if(total_income>total_cost) return i;
+	else {
+		result = a*(c-b)+(c-b);
+		return result;
 	}
 
 }
+	
+
+
 
 int main(){
-	unsigned long A,B,C;
+	long A,B,C;
 	cin >> A >> B >> C ;
-	unsigned long result = Break_Even_Point(A,B,C);
+	long result = Break_Even_Point(A,B,C);
 	cout << result << endl;
 }
