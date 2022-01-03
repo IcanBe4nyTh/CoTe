cfa41839f78bc4b082d66b8476dba83347290873
sw <4nyth4me@gmail.com>
2022-01-04 01:58:38 +0900

    check

--- 10828.py
+++ 10828.py
@@ -3,7 +3,7 @@ N=int(input())
 stack = []
 
 for i in range(0,N):
-    order1=sys.stdin.readline().strip()
+    order1=sys.stdin.readline().rstrip()
     order = order1.split(' ')[0]
     if order == 'push' :
         num = int(order1.split(' ')[1])
@@ -27,5 +27,10 @@ for i in range(0,N):
 
 """
 order1에 input()을 넣으니까 시간초과가 나왔길래 sys를 써보았다
-sys공부해봐야겠
+sys공부해봐야겠다
+---------------------------------------------------------
+참고 -- https://velog.io/@gouz7514/%ED%8C%8C%EC%9D%B4%EC%8D%AC-input-vs-sys.stdin.readline
+반복문에서 input을 사용했을때 시간초과가 나올 수 있음 
+관련문제 백준 15552번 
+
 """
